5342825fcb808d6ac9495046e1e3a359c9e98dc7
SwapnilAndhare1 <flutter.swapnil@gmail.com>
2024-01-30 18:56:12 +0530

    Basic Project

--- Practice Apps/portfolio/lib/portfoilo.dart
+++ Practice Apps/portfolio/lib/portfoilo.dart
@@ -1,107 +1,129 @@
 import 'package:flutter/material.dart';
 
 class portfolio extends StatefulWidget{
-  
+  const portfolio({super.key});
+
   @override
-  State<StatefulWidget> createState() => _portfolio();
+  State<portfolio> createState() => _portfolio();
 }
 
 class _portfolio extends State<portfolio>{
+  int counter = 0;
+
+  void increment(){
+    setState(() {
+      counter = counter + 1;
+    });
+  }
   
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar( 
         title:const Text(
-          'Portfolio',
+          'PORTFOLIO',
+          style: TextStyle(
+            fontSize: 25,
+          ),
         ),
         backgroundColor: Colors.black,
+        centerTitle: true,
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Container(
-            height: 20,
-          ),
-            const Row(
+          (counter>=1)? const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
                 Text(
                 "Name:- Swapnil Andhare",
                 style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
                 ),
               )
             ],
+          ):Container(
+            color: Colors.white,
+            height: 0,
+            width: 0,
           ),
-          Container(
-            height: 20,
-          ),
-          Row(
+
+          (counter>=2)?Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 50,
-              ),
               Container(
                 height: 150,
                 width: 150,
-                child:Image.asset("assets/images/2.jpg")
+                color: Colors.white,
+                child:Image.asset("assets/images/2.jpg"),
               )
             ],
+          ):Container(
+            color: Colors.white,
+            height: 0,
+            width: 0,
           ),
-          Container(
-            height: 20,
-          ),
-          Row(
+          (counter>=3)?const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: 50,
-              ),
-              const Text(
+                Text(
                 "College:- SKNCOE",
                 style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
                 ),
               ),
             ],
-          ),
-          Row(
+          ):Container(height: 0),
+
+          (counter>=4)?Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                width: 30,
-              ),
-              Container(
+                color: Colors.white,
                 height: 200,
                 width: 200,
                 child:Image.network("https://tse4.mm.bing.net/th?id=OIP.72Lu0ID3FKWYIOrBrSz4EQHaEc&pid=Api&P=0&h=220"),
               )
             ],
-          ),
-          const Row(
+          ):Container(height: 0,),
+
+          (counter>=5)?const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 "Dream Company:- Microsoft",
                 style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
                 ),
               )
             ],
+          ):Container(
+            height: 0,
           ),
-          Row(
+          (counter>=6)? Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                width: 30,
-              ),
-              Container(
+                color: Colors.white,
                 height: 200,
                 width: 200,
                 child:Image.network("https://tse2.mm.bing.net/th?id=OIP.00E0zwyOfqXB5nj0r2bvSAHaE1&pid=Api&P=0&h=220"),
               )
             ],
+          ):Container(
+            height: 0,
           )
         ],
       ),
+      floatingActionButton: FloatingActionButton(onPressed: increment,
+        mini: false,
+        mouseCursor:MaterialStateMouseCursor.clickable,
+        backgroundColor: Colors.black,
+        splashColor: Colors.amber,
+        foregroundColor: Colors.amber,
+        hoverColor: Colors.green,
+        focusColor: Colors.pink,
+        child: const Text("+")),
     );
   }
 }
